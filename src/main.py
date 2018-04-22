81f6d31ac4535d725e22c56899d85ae6bfcc4dc8
keithfleming <36519357+keithfleming@users.noreply.github.com>
2018-04-22 00:15:48 -0400

    Added elastic net regression model

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import data.ff as data
+import models.train_model as model
+
+if __name__ == "__main__":
+    fantasy2013 = data.load_data("2013_Fantasy")
+    fantasy2014 = data.load_data("2014_Fantasy")
+    fantasy2015 = data.load_data("2015_Fantasy")
+
+    QB_2013_2014_index = data.get_index_for_position(fantasy2013,fantasy2014)
+    QB_2014_2015_index = data.get_index_for_position(fantasy2014, fantasy2015)
+
+    QBstats2013, QBpoints2014 = data.prepare_input_data(fantasy2013, fantasy2014,  QB_2013_2014_index, 'QB')
+    QBstats2014, QBpoints2015 = data.prepare_input_data(fantasy2014, fantasy2015, QB_2014_2015_index, 'QB')
+
+    QB_linear_model, QB_linear_preds, QB_linear_mse = model.linear_regression(QBstats2013, QBpoints2014, QBstats2014, QBpoints2015)
+    QB_ridge_model, QB_ridge_preds, QB_ridge_mse = model.ridge_regression(QBstats2013, QBpoints2014, QBstats2014, QBpoints2015)
+    QB_lasso_model, QB_lasso_preds, QB_lasso_mse = model.lasso_regression(QBstats2013, QBpoints2014, QBstats2014, QBpoints2015)
+    QB_elasticnet_model, QB_elasticnet_preds, QB_elasticnet_mse = model.elasticnet_regression(QBstats2013, QBpoints2014, QBstats2014,                                                          QBpoints2015)
+    QB_knn_model, QB_knn_preds, QB_knn_mse = model.knn(QBstats2013, QBpoints2014, QBstats2014, QBpoints2015)
---
+++ src/models/train_model.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+from sklearn  import linear_model
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import cross_val_predict
+
+def linear_regression(training_stats, training_future_points, test_stats, test_future_points):
+    """
+        Fit and test a linear regression model
+        :param training_stats: The dataframe with player statistics to fit the model
+        :param training_future_points: The set of future fantasy points for the training_stats
+        :param test_stats: The dataframe with player statistics to test model
+        :param test_future_points: The set of future fantasy points for the test_stats
+        :return: Model intercept, model coefficients, and mse from the test set
+        """
+    model = linear_model.LinearRegression()
+    model.fit(training_stats, training_future_points)
+
+    pred = model.predict(test_stats)
+
+    mse = metrics.mean_squared_error(test_future_points, pred)
+
+    return model, pred, mse
+
+def ridge_regression(training_stats, training_future_points, test_stats, test_future_points):
+    """
+        Fit and test a ridge regression model
+        :param training_stats: The dataframe with player statistics to fit the model
+        :param training_future_points: The set of future fantasy points for the training_stats
+        :param test_stats: The dataframe with player statistics to test model
+        :param test_future_points: The set of future fantasy points for the test_stats
+        :return: Ridge regression model and MSE from the test set
+        """
+    model = linear_model.RidgeCV()
+    model.fit(training_stats, training_future_points)
+
+    pred = model.predict(test_stats)
+
+    mse = metrics.mean_squared_error(test_future_points, pred)
+
+    return model, pred, mse
+
+def lasso_regression(training_stats, training_future_points, test_stats, test_future_points):
+    """
+        Fit and test a lasso regression model
+        :param training_stats: The dataframe with player statistics to fit the model
+        :param training_future_points: The set of future fantasy points for the training_stats
+        :param test_stats: The dataframe with player statistics to test model
+        :param test_future_points: The set of future fantasy points for the test_stats
+        :return: Lasso regression model and MSE from the test set
+        """
+    model = linear_model.LassoCV()
+    model.fit(training_stats, training_future_points)
+
+    pred = model.predict(test_stats)
+
+    mse = metrics.mean_squared_error(test_future_points, pred)
+
+    return model, pred, mse
+
+def elasticnet_regression(training_stats, training_future_points, test_stats, test_future_points):
+    """
+        Fit and test a elastic net regression model
+        :param training_stats: The dataframe with player statistics to fit the model
+        :param training_future_points: The set of future fantasy points for the training_stats
+        :param test_stats: The dataframe with player statistics to test model
+        :param test_future_points: The set of future fantasy points for the test_stats
+        :return: Elastic Net regression model and MSE from the test set
+        """
+    model = linear_model.ElasticNetCV()
+    model.fit(training_stats, training_future_points)
+
+    pred = model.predict(test_stats)
+
+    mse = metrics.mean_squared_error(test_future_points, pred)
+
+    return model, pred, mse
+
+def knn(training_stats, training_future_points, test_stats, test_future_points):
+    """
+        Fit and test a k nearest neighbor model
+        :param training_stats: The dataframe with player statistics to fit the model
+        :param training_future_points: The set of future fantasy points for the training_stats
+        :param test_stats: The dataframe with player statistics to test model
+        :param test_future_points: The set of future fantasy points for the test_stats
+        :return: k nearest neighbors and MSE from the test set
+        """
+    neighbors = [2,4,6,8,10,12,14,16,18,20]
+
+    cv_scores = []
+
+    for k in neighbors:
+        knn = KNeighborsRegressor(n_neighbors = k)
+        scores = cross_val_predict(knn, training_stats, training_future_points, cv = 5)
+        cv_scores.append(scores.mean())
+
+    optimal_k = neighbors[cv_scores.index(min(cv_scores))]
+
+    model = KNeighborsRegressor(n_neighbors=optimal_k)
+    model.fit(training_stats, training_future_points)
+
+    pred = model.predict(test_stats)
+
+    mse = metrics.mean_squared_error(test_future_points, pred)
+
+    return model, pred, mse
