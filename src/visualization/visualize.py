d08ec249e705b4a55f81f6cb97e649a033899d11
keithfleming <36519357+keithfleming@users.noreply.github.com>
2018-04-23 22:28:42 -0400

    Finalized plot designs

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,6 +8,7 @@ def plot_yards_by_position(df):
     :param df: The dataframe with a season of data
     :return: None
     """
+    plt.figure()
     positiondf = df.groupby('FantPos').mean()
     ax = positiondf[['PassYds', 'RushYds', 'RecYds']].plot(kind = 'bar', stacked = True, title = 'Total Yards by Position')
     ax.set_xlabel('Position')
@@ -20,12 +21,14 @@ def hist_yards_by_pos(df, pos = 'QB'):
     :param pos: The position to create the histogram for
     :return: None
     """
+    plt.figure()
     df = df[df['FantPos'] == pos]
     ax = df[['PassYds', 'RushYds', 'RecYds']].plot.hist(bins = 25, stacked = True, title = 'Histogram of Yardage for ' + pos)
     ax.set_ylabel('Frequency')
     ax.set_xlabel('Yards')
 
 def plot_regression_coefs(multiple, ridge, lasso, elasticnet, pos):
+    plt.figure()
     width = 0.2
 
     if pos == 'QB':
@@ -40,35 +43,40 @@ def plot_regression_coefs(multiple, ridge, lasso, elasticnet, pos):
     r3 = [x + width for x in r2]
     r4 = [x + width for x in r3]
 
-    plt.bar(r1, multiple, color = 'r', width = width, label = 'Linear')
+    plt.bar(r1, multiple, color = '#cc0000', width = width, label = 'Linear')
     plt.bar(r2, ridge, color = 'b', width = width, label = 'Ridge')
     plt.bar(r3, lasso, color='g', width=width, label='Lasso')
-    plt.bar(r4, elasticnet, color='y', width=width, label='Elastic Net')
+    plt.bar(r4, elasticnet, color='#ff5500', width=width, label='Elastic Net')
 
     plt.style.use('seaborn')
     plt.xticks(rotation=45)
-    plt.title(pos + ' Regression Model Coefficients')
-    plt.xlabel('Coefficient')
-    plt.ylabel('Coefficient Value')
-    plt.xticks([r + width for r in range(len(multiple))], labels)
+    plt.title(pos + ' Regression Model Coefficients', size = 14)
+    plt.xlabel('Coefficient', size = 12)
+    plt.ylabel('Coefficient Value', size = 12)
+    plt.xticks([r + width for r in range(len(multiple))], labels, size = 12)
+    plt.tight_layout()
 
-    plt.legend()
+    legend = plt.legend(frameon=1, prop={'size': 12})
+    frame = legend.get_frame()
+    frame.set_color('white')
     plt.show()
 
 def plot_pred_vs_actual(pred, actual, pos, model):
+    plt.figure()
     extrema = max(max(pred), max(actual))
     extrema = int(ceil(extrema / 100.0)) * 100
 
     plt.plot([-20,extrema+20], [-20,extrema+20], '--', color = 'b')
-    plt.scatter(pred, actual)
+    plt.scatter(pred, actual, color = 'r')
     plt.axis('equal')
 
     plt.ylim(0,extrema)
     plt.xlim(0,extrema)
 
-    plt.xlabel('Predicted Values')
-    plt.ylabel('Actual Values')
-    plt.title(pos + ' ' + model + ' Model Predicted vs. Actual')
+    plt.xlabel('Predicted Values', size = 12)
+    plt.ylabel('Actual Values', size = 12)
+    plt.title(pos + ' ' + model + ' Model Predicted vs. Actual', size = 14)
+    plt.tight_layout()
 
     plt.show()
 
